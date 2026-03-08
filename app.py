b9c5243b5828e30f47e27b338f92e0a5c608d3ea
JAFGT <jfluss6@gatech.edu>
2026-03-08 00:56:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,27 +6,31 @@ st.set_page_config(layout="wide")
 
 st.markdown("""
 <style>
-/* Page Background */
-[data-testid="stAppViewContainer"] {
-    background: linear-gradient(135deg, #0f0c29 0%, #0a203c 50%, #05172a 100%);
-    color: #ffffff;
-}
 
-/* Header Transparency */
+/* PAGE BACKGROUND */
+[data-testid="stAppViewContainer"] {background: linear-gradient(135deg, #0f0c29 0%, #0a203c 50%, #05172a 100%); color: #ffffff;}
+
+/* HEADER TRANSPARENCY */
 [data-testid="stHeader"] {background: rgba(0,0,0,0);}
 
-/* Containers targeted by key */
+/*CONTAINER STYLES*/
 .st-key-blc1, .st-key-blc2, .st-key-blc3, .st-key-blc4 {
-    background-color: #102f54;
-    border: 2px solid #b0a36f;
-    border-radius: 12px; 
-    padding: 20px; 
-    margin-bottom: 20px;
+    background-color: #102f54; border: 2px solid #b0a36f; border-radius: 12px; padding: 20px; margin-bottom: 20px; 
     backdrop-filter: blur(5px); /* Adds a nice frosted glass effect */
 }
 
+/* BUTTON */
+.stButton>button:active {background-color: #b0a36f; color: #102f54;}
+
 </style>
 """, unsafe_allow_html=True) 
+
+#ENERGY LOAD SECTORS
+sectors = ["Airport Terminal", "GSE", "Manufacturing Plant", "Other Facilities"]
+for sector in sectors:
+    if sector not in st.session_state:
+        st.session_state[sector] = False  # unchecked by default
+
 
 st.title("Airport Dashboard")
 
@@ -37,8 +41,11 @@ col1, col2, col3 = st.columns([1,1,1], gap="medium")
 with col1:
     with st.container(key="blc1"):
         st.write("### Gate A")
-        gate_a = st.checkbox("Gate A")
         st.slider("Capacity A", 0, 100, 50)
+        for sectors
+            label = f"✅ {sector}" if st.session_state[sector] else sector
+            if st.button(label, key=f"btn_{sector}"):
+                st.session_state[sector] = not st.session_state[sector]
 
 with col2:
     with st.container(key="blc2"):
