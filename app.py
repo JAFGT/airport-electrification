956d6b323ceb7fec19aa14583aa5d8c9126ecbdf
JAFGT <jfluss6@gatech.edu>
2026-03-07 23:27:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # 1. Simple CSS: Target any container with the key "blue_box"
 st.markdown("""
 <style>
- st-key-blc {background-color: rgba(100, 100, 200, 0.3);
+ .st-key-blc {background-color: rgba(100, 100, 200, 0.3);
  border: 2px solid #3498db; 
  /* Blue border */ border-radius: 8px; 
  /* Rounded corners */ padding: 20px; 
