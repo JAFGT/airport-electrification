9bb17766f5daf8c673ddac05e0c49eca9d66bd66
JAFGT <jfluss6@gatech.edu>
2026-03-08 14:21:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown("""
         color: #b0a36f;
         border: 1px solid #b0a36f;
         border-radius: 8px;
-        width: 100%;
+        width: auto;
         padding: 10px 20px;
         font-weight: 600;
         transition: all 0.3s ease;
